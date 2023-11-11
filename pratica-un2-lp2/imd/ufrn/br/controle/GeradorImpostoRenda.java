0d8f1454062d62c33029c8d647ca7a3f3ed5ad33
LucasNCortez <lucascortez6@hotmail.com>
2023-11-11 20:20:43 -0300

    GeradorImpostoDeRenda Implemented

--- pratica-un2-lp2/imd/ufrn/br/controle/GeradorImpostoRenda.java
+++ pratica-un2-lp2/imd/ufrn/br/controle/GeradorImpostoRenda.java
@@ -1,5 +1,25 @@
 package imd.ufrn.br.controle;
 
-public class GeradorImpostoRenda {
+import imd.ufrn.br.modelo.Pessoa;
 
+public class GeradorImpostoRenda {
+  private double totalTibuto;
+
+  public GeradorImpostoRenda(){
+
+  }
+
+  public double getTotalTributo(){
+    return this.totalTibuto;
+  }
+
+  public double calcularValorTotalTributo(Pessoa pessoa){
+    this.totalTibuto = 0;
+
+    this.totalTibuto += pessoa.calcularTributos();
+    this.totalTibuto += pessoa.getConta().calcularTributos();
+    this.totalTibuto += pessoa.getSeguro().calcularTributos();
+
+    return this.totalTibuto;
+  }
 }
