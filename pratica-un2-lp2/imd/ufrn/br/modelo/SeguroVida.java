33cbcb4b741964f00273e5ea889e3037a36ad702
ribmen <118929790+ribmen@users.noreply.github.com>
2023-11-12 20:45:38 -0300

    SeguroVida.java

--- pratica-un2-lp2/imd/ufrn/br/modelo/SeguroVida.java
+++ pratica-un2-lp2/imd/ufrn/br/modelo/SeguroVida.java
@@ -1,5 +1,38 @@
 package imd.ufrn.br.modelo;
 
-public class SeguroVida {
+public class SeguroVida implements ITributavel{
 
+    private int numero;
+    private String beneficiado;
+    private double valor;
+    private static final double taxaFixaSeguroVida = 31.50;
+
+    public int getNumero(){
+        return numero;
+    }
+    public void setNumero(int numero){
+        this.numero = numero;
+    }
+
+    public String getBeneficiado() {
+        return beneficiado;
+    }
+
+    public void setBeneficiado(String beneficiado) {
+        this.beneficiado = beneficiado;
+    }
+
+    public double getValor() {
+        return valor;
+    }
+
+    public void setValor(double valor) {
+        this.valor = valor;
+    }
+
+    public static double getTaxaFixaSeguroVida() {
+        return taxaFixaSeguroVida;
+    }
+
+    public double salarioTaxaDescontada(double getSalario());
 }
