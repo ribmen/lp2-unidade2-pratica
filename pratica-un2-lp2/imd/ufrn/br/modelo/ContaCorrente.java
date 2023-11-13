63fad579ffc904331957e701d47e942ae42b438c
ribmen <118929790+ribmen@users.noreply.github.com>
2023-11-12 21:09:27 -0300

    ContaCorrente.java

--- pratica-un2-lp2/imd/ufrn/br/modelo/ContaCorrente.java
+++ pratica-un2-lp2/imd/ufrn/br/modelo/ContaCorrente.java
@@ -1,5 +1,85 @@
 package imd.ufrn.br.modelo;
 
-public class ContaCorrente {
+import java.util.Scanner;
+import java.io.*;
+import imd.ufrn.br.modelo.SeguroVida;
+import imd.ufrn.br.modelo.Pessoa;
 
+public class ContaCorrente implements ITributavel{
+
+    private String agencia;
+    private String numero;
+    private double saldo;
+    private static final double taxaContaCorrente = 0.0038; //final??
+    
+    public String getAgencia(){
+        return agencia;
+    }
+    public void setAgencia(String agencia){
+        this.agencia = agencia;
+    }
+
+    public double getSaldo(double saldo){
+        return saldo;
+    }
+    public void setSaldo(){
+        this.saldo = saldo;
+    }
+
+    public double jurosCDB(double getSalario()){
+        salarioInvestimento = getSaldo() * (1 + taxaContaCorrente);
+        return salarioInvestimento;
+    }
+
+    double calcularTributos(double getSalario()) {
+        double salarioTributado = getSaldo() - (getSaldo() * taxaSalario);
+        return salarioTributado;
+    }
+
+    double salarioTaxaDescontada(double getSalario()) {
+            void salarioDescontado = getSalario() - taxaFixaSeguroVida;
+            return salarioDescontado;
+        }
+
+    void sacar(double valor) {
+        Scanner s = new Scanner(System.in);
+        valorDebitado = ScannerObject.nextDouble();
+        System.out.println("Insira o valor a ser debitado da conta: ");
+
+        if(valorDebitado < getSalario()){
+            salarioDebitado = getSalario() - valorDebitado;
+            System.out.println("Seu saldo é R$" + salarioDebitado);
+        } else if(valorDebitado > getSalario()) {
+            System.out.println("Salário insuficiente");
+        } else {
+            System.out.println("Insira um valor a ser debitado");
+        }
+
+    }
+    void depositar(double valor){
+        Scanner s = new Scanner(System.in);
+        valorDepositado = ScannerObject.nextDouble();
+        System.out.println("Insira o valor a ser depositado da conta: ");
+        if(valorDepositado > 0) {
+            salarioDepositado = getSalario() + valorDepositado;
+            System.out.println("Seu saldo atual é R$" + salarioDepositado);
+        } else {
+            System.out.println("Insira valores válidos.");
+        }
+    }
+    boolean transiferir(double valor, ContaCorrente cc) {
+        Scanner s = new Scanner(System.in);
+        valorTransferido = ScannerObject.nextDouble();
+        System.out.println("Insira o valor a ser transferido da conta: ");
+        if(valorDepositado > 0) {
+            System.out.println("Insira a conta a ser transferido o valor: ");
+            Scanner t = new Scanner(System.in);
+            Conta = ScannerObject.nextString();
+
+        }
+
+    } else {
+            System.out.println("Insira valores válidos.");
+        }
 }
+
